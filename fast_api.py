697f09be14918f4e01299941e02d29ad344c39f7
Alex-Vay <vatlin04@mail.ru>
2024-12-01 03:35:39 +0500

    Поработал над добавлением элементов

--- fast_api.py
+++ fast_api.py
@@ -1,19 +1,17 @@
 import asyncio
-from fastapi import FastAPI, BackgroundTasks, Depends, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
 from mvideo import get_data_mvideo
-from starlette.concurrency import run_in_threadpool
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-app = FastAPI()
-
 class Prices(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int
     name: str
     cost: int
-    link: str
+    link: str = Field(primary_key=True)
 
+
+app = FastAPI()
 sqlite_url = "sqlite:///parser.db"
 engine = create_engine(sqlite_url)
 
@@ -23,11 +21,14 @@ def get_async_session():
     dbsession = async_sessionmaker(engine_2)
     return dbsession()
 
+
 async def get_session():
     async with get_async_session() as session:
         yield session
 
+
 SessionDep = Depends(get_session)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -36,44 +37,30 @@ def create_db_and_tables():
 async def background_parser_async():
     while True:
         print("Starting get price")
-        # await asyncio.sleep(12 * 60 * 60)
-        data = await run_in_threadpool(get_data_mvideo)
-        session = get_async_session()
-        for i in data:
-            print(i)
-            existing_product = await session.execute(
-                select(Prices).where(Prices.link == i["link"])
-            )
-            if existing_product.scalar_one_or_none() is None:
-                # Если продукт не найден, добавляем его
-                p = Prices(**i)
-                session.add(p)
-                await session.commit()
-
-        # add_item(title, price, index)
+        async with get_async_session() as session:
+            async for product in get_data_mvideo():
+                existing_product = await session.execute(
+                    select(Prices).where(Prices.link == product["link"])
+                )
+                if existing_product.scalar_one_or_none() is None:
+                    p = Prices(**product)
+                    session.add(p)
+                    await session.commit()
         await asyncio.sleep(12 * 60 * 60)
 
-# def background_add_item():
-#     data = get_price()
-#     # add_item(*data)
 
 @app.on_event("startup")
 async def startup_event():
     create_db_and_tables()
     asyncio.create_task(background_parser_async())
 
-# @app.get("/start_parser")
-# async def start_parser(background_tasks: BackgroundTasks):
-#     #  asyncio.create_task(background_add_item())
-#     background_tasks.add_task(background_add_item)
-#     return {}
 
 @app.get("/prices")
 async def read_prices(session: Session = SessionDep, offset: int = 0, limit: int = 100):
-    # return session.exec(select(Prices).offset(offset).limit(limit)).all()
     stmt = select(Prices).offset(offset).limit(limit)
     items = await session.scalars(stmt)
     return items.all()
+
 
 @app.get("/prices/{item_id}")
 async def read_item(item_id: int, session: Session = SessionDep):
@@ -81,6 +68,7 @@ async def read_item(item_id: int, session: Session = SessionDep):
     if not price:
         raise HTTPException(status_code=404, detail="Price not found")
     return price
+
 
 @app.put("/prices/{item_id}")
 async def update_item(item_id: int, data: Prices, session: Session = SessionDep):
@@ -91,8 +79,9 @@ async def update_item(item_id: int, data: Prices, session: Session = SessionDep)
     price_db.sqlmodel_update(price_data)
     session.add(price_db)
     await session.commit()
-    session.refresh(price_db)
+    await session.refresh(price_db)
     return price_db
+
 
 @app.post("/prices/create")
 async def create_item(item: Prices, session: Session = SessionDep):
@@ -100,6 +89,7 @@ async def create_item(item: Prices, session: Session = SessionDep):
     await session.commit()
     await session.refresh(item)
     return item
+
 
 @app.delete("/prices/{item_id}")
 async def delete_item(item_id: int, session: Session = SessionDep):
--- mvideo.py
+++ mvideo.py
@@ -1,9 +1,11 @@
-import math, time
+import asyncio
+import math
 import requests
+
 from config import headers, cookies
 
 
-def get_data_mvideo():
+async def get_data_mvideo():
     ids = 118 #Ноутбуки
     params = {
         'categoryId': f'{ids}',
@@ -18,7 +20,7 @@ def get_data_mvideo():
     total_items = resp['body']['total']
     total_pages = math.ceil(total_items / 24)
     print(f'[INFO] Total positions: {total_items} | Total pages: {total_pages}')
-    for i in range(total_pages // 20):
+    for i in range(total_pages):
         try:
             products_desc = {}
             products_prices = {}
@@ -38,7 +40,8 @@ def get_data_mvideo():
                                 json=data)
             tr = 1
             while resp.status_code != 200 and tr < 5:
-                time.sleep(30)  #время сна, так как иногда запросы не проходят, так получается спарсить всю категорию
+                await asyncio.sleep(30)  #время сна, так как иногда запросы не проходят, так получается спарсить всю категорию
+                # time.sleep(30)
                 resp = session.post('https://www.mvideo.ru/bff/product-details/list', cookies=cookies, headers=headers,
                                     json=data)
                 tr += 1
@@ -63,7 +66,6 @@ def get_data_mvideo():
                     }
                 print(f'[+] Success get {i + 1} of the {total_pages} pages success')
                 n = 0
-                prod = []
                 for items in products_desc.values():
                     products = items['body']['products']
                     for item in products:
@@ -77,73 +79,12 @@ def get_data_mvideo():
                             n = 0
                         else:
                             n += 1
-                        # print(name, current_price, link)
-                        prod.append({
+                        yield {
                             'id' : i * 24 + n,
                             'name' : name,
                             'cost' : int(current_price),
-                            'link' : link})
-                return prod
+                            'link' : link}
             else:
                 print(f'[!] Skipped {i + 1} page')
         except Exception as e:
             print(f'[!] Skipped {i + 1} page', e.__class__.__name__)
-
-
-
-
-    # with open('data/1_product_description.json', 'w', encoding='UTF-8') as file:
-    #     json.dump(products_desc, file, indent=4, ensure_ascii=False)
-    # with open('data/2_product_prices.json', 'w', encoding='UTF-8') as file:
-    #     json.dump(products_prices, file, indent=4, ensure_ascii=False)
-
-
-# def get_result(worksheet, center):
-#     row = 1
-#
-#     with open('data/1_product_description.json', 'r', encoding='UTF-8') as file:
-#         products_desc = json.load(file)
-#     with open('data/2_product_prices.json', 'r', encoding='UTF-8') as file:
-#         products_prices = json.load(file)
-#
-#             worksheet.write(row, 0, name, center)
-#             worksheet.write(row, 1, current_price, center)
-#             worksheet.write(row, 2, link)
-#             if n == len(items['body']['products']) - 1:
-#                 n = 0
-#             else:
-#                 n += 1
-#             row += 1
-
-
-# def get_categoryId(name):
-#     ids = {
-#         'Ноутбуки': '118',
-#         'Смартфоны': '205',
-#         'Планшеты': '195',
-#     }
-#     item_id = ids[name]
-#     return item_id
-
-
-# def parse(category):
-#     if not os.path.exists('output'):
-#         os.mkdir('output')
-#     path = f'output\\{category}-{datetime.datetime.now().strftime("%d-%m-%Y")}.xlsx'
-#     workbook = Workbook(path)
-#     format = workbook.add_format({'bold': True, 'align': 'center'})
-#     center = workbook.add_format({'align': 'center'})
-#     worksheet = workbook.add_worksheet()
-#     worksheet.write(0, 0, 'Название', format)
-#     worksheet.write(0, 1, 'Цена', format)
-#     worksheet.write(0, 2, 'Ссылка на товар', format)
-#     get_data_mvideo(get_categoryId(category))
-#     get_result(worksheet, center)
-#     worksheet.autofit()
-#     workbook.close()
-#     return path
-
-
-# parse('Ноутбуки')
-for i in get_data_mvideo():
-    print(i)
